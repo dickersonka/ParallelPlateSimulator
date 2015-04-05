dc66e6159d971d623c4226f162c2795855cd28ff
shuhaokou <shuhao501@yahoo.com>
2015-04-05 01:56:02 -0500

    calculator

---
+++ src/calculator/Calculation.java
@@ -0,0 +1,32 @@
+package calculator;
+
+public class Calculation {
+	Calculation(){
+		
+	}
+	
+	private double e0 = 8.854;
+	
+	public double calculate(int missingtwo, double area, double capacitance, double k, double distance){
+		if(missingtwo >= 2){
+			return 0;
+		}
+		else{
+			if(area == 0){
+				return capacitance * distance / e0 / k;
+			}
+			else if(capacitance == 0){
+				return k * e0 * area / distance;
+			}
+			else if(k == 0){
+				return capacitance * distance / e0 / area;
+			}
+			else if(distance == 0){
+				return k * e0 * area / capacitance;
+			}
+		}
+		return 1;
+		
+	}
+	
+}
---
+++ src/calculator/Controller.java
@@ -0,0 +1,121 @@
+package calculator;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class Controller {
+	@FXML
+	Button Area;
+	@FXML
+	Button K;
+	@FXML
+	Button Capacity;
+	@FXML
+	Button Distance;
+	@FXML
+	Button equal;
+	@FXML
+	TextField resultArea;
+	@FXML
+	TextField resultK;
+	@FXML
+	TextField resultCapacity;
+	@FXML
+	TextField resultDistance;
+	@FXML
+	TextField questions;
+	
+	
+	private double e0 = 8.854;
+	private String e = " x 10^(-12) F / m";
+	private double area;
+	private double capacitance;
+	private double distance;
+	private double k;
+	private int morethantwomissing = 0;
+	
+	Calculation c = new Calculation();
+	
+	@FXML
+	private void initialize(){
+		
+
+	}
+	
+	@FXML
+	private void runCalculator(){
+		if(!isAreaEmpty()){
+			area = Double.parseDouble(resultArea.getText());
+		}else{
+			morethantwomissing++;
+			area = 0;
+		}
+		if(!isCapacityEmpty()){
+			capacitance = Double.parseDouble(resultCapacity.getText());
+		}else{
+			morethantwomissing++;
+			capacitance = 0;
+		}
+		if(!isKEmpty()){
+			k = Double.parseDouble(resultK.getText());
+		}else{
+			morethantwomissing++;
+			k = 0;
+		}
+		if(!isDistanceEmpty()){
+			distance = Double.parseDouble(resultDistance.getText());
+		}else{
+			morethantwomissing++;
+			distance = 0;
+		}
+		getResult();
+	}
+	
+	private void getResult(){
+		if(getValue() == 0){
+			questions.setText("need more information"); 
+		}
+		else if(getValue() == 1){
+			questions.setText("You already put 4 values in there");
+		}
+		else{
+			if(area == 0){
+				resultArea.setText(Double.toString(getValue()));
+			}
+			if(capacitance == 0){
+				resultCapacity.setText(Double.toString(getValue()));
+			}
+			if(k == 0){
+				resultK.setText(Double.toString(getValue()));
+			}
+			if(distance == 0){
+				resultDistance.setText(Double.toString(getValue()));
+			}
+			questions.clear();
+		}
+		morethantwomissing = 0;
+	}
+	
+	private double getValue(){
+		return c.calculate(morethantwomissing, area, capacitance, k, distance);
+	}
+	
+	private boolean isAreaEmpty(){
+		return resultArea.getText().equals("");
+	}
+	
+	private boolean isCapacityEmpty(){
+		return resultCapacity.getText().equals("");
+	}
+	
+	private boolean isKEmpty(){
+		return resultK.getText().equals("");
+	}
+	
+	private boolean isDistanceEmpty(){
+		return resultDistance.getText().equals("");
+	}
+	
+	
+}
