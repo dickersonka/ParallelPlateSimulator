286a2966f1a3e9c1518cc8dcc0b3a0c49d9ebeca
dickersonka <kdickerson95@gmail.com>
2015-04-22 16:22:41 -0500

    Setting up some basic classes

---
+++ src/gui/Controller.java
@@ -0,0 +1,11 @@
+package gui;
+
+import javafx.fxml.FXML;
+
+public class Controller {
+
+	@FXML
+	public void initialize() {
+		
+	}
+}
---
+++ src/gui/Main.java
@@ -0,0 +1,28 @@
+package gui;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.TabPane;
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(Main.class.getResource("Simulator.fxml"));
+			TabPane root = (TabPane) loader.load();
+			Scene scene = new Scene(root,640,500);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace(); 
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
